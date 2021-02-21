c5cf241d7bd725321e6b828181fa5ee2d84f7e3f
Marcia <marciaadler718@gmail.com>
2021-02-21 14:34:18 -0500

    added back reportwebvitals.js file

--- routes/food.js
+++ routes/food.js
@@ -3,8 +3,6 @@ const router = require("express").Router();
 const key = process.env.FOOD_APIKEY;
 const query = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.FOOD_APIKEY}`;
 router.get("/api/food/:food", (req, res) => {
-  console.log("params", req.params.food);
-  console.log("query", query);
   const queryURL = query + "&query=" + req.params.food;
   axios
     .get(queryURL)
